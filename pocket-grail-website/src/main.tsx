83247da067f4a09394ba293df355a508aa831160
Heley Danylo <78755636+StoRmGC228@users.noreply.github.com>
2026-04-27 16:55:53 +0300

    feat/add-home-page

--- pocket-grail-website/src/main.tsx
+++ pocket-grail-website/src/main.tsx
@@ -2,14 +2,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 
 document.documentElement.setAttribute('data-theme', 'dark')
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</StrictMode>,
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
 )
